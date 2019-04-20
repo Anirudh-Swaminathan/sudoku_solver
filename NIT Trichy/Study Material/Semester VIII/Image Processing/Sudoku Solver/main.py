c3374927565cb90d19d25ad0b55ef7eeb492e996
Anirudh Swaminathan <aniswami97@gmail.com>
2019-04-21 00:09:30 +0530

    Binarized and resized sudoku into 81 images
    Binarized using Gaussian Blurring adaptive thresholding
    Resized using interpolation technique from 40*40 to 28*28

--- NIT Trichy/Study Material/Semester VIII/Image Processing/Sudoku Solver/main.py
+++ NIT Trichy/Study Material/Semester VIII/Image Processing/Sudoku Solver/main.py
@@ -2,64 +2,68 @@ import cv2
 import numpy as np
 import operator
 
+
 def findCorners(img):
+    _, contours, h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    polygon = contours[0]
 
-	_, contours, h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	contours = sorted(contours, key=cv2.contourArea, reverse=True)
-	polygon = contours[0]
+    bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+    top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+    bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+    top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
 
-	bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-	top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-	bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-	top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+    return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
 
-	return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
 
 def displayRects(inImg, rects, colour=255):
-	img = inImg.copy()
-	for rect in rects:
-		img = cv2.rectangle(img, tuple(int(x) for x in rect[0]), tuple(int(x) for x in rect[1]), colour)
-	showImage(img)
-	return img
+    img = inImg.copy()
+    for rect in rects:
+        img = cv2.rectangle(img, tuple(int(x) for x in rect[0]), tuple(int(x) for x in rect[1]), colour)
+    showImage(img)
+    return img
+
 
 def distance(p1, p2):
-	a = p2[0] - p1[0]
-	b = p2[1] - p1[1]
-	return np.sqrt((a ** 2) + (b ** 2))
+    a = p2[0] - p1[0]
+    b = p2[1] - p1[1]
+    return np.sqrt((a ** 2) + (b ** 2))
+
 
 def crop(img, crop_rect):
+    top_left, top_right, bottom_right, bottom_left = crop_rect[0], crop_rect[1], crop_rect[2], crop_rect[3]
+    src = np.array([top_left, top_right, bottom_right, bottom_left], dtype='float32')
 
-	top_left, top_right, bottom_right, bottom_left = crop_rect[0], crop_rect[1], crop_rect[2], crop_rect[3]
-	src = np.array([top_left, top_right, bottom_right, bottom_left], dtype='float32')
+    side = max([
+        distance(bottom_right, top_right),
+        distance(top_left, bottom_left),
+        distance(bottom_right, bottom_left),
+        distance(top_left, top_right)
+    ])
 
-	side = max([
-		distance(bottom_right, top_right),
-		distance(top_left, bottom_left),
-		distance(bottom_right, bottom_left),
-		distance(top_left, top_right)
-	])
+    dst = np.array([[0, 0], [side - 1, 0], [side - 1, side - 1], [0, side - 1]], dtype='float32')
+    m = cv2.getPerspectiveTransform(src, dst)
 
-	dst = np.array([[0, 0], [side - 1, 0], [side - 1, side - 1], [0, side - 1]], dtype='float32')
-	m = cv2.getPerspectiveTransform(src, dst)
+    return cv2.warpPerspective(img, m, (int(side), int(side)))
 
-	return cv2.warpPerspective(img, m, (int(side), int(side)))
 
 def gridDetect(img):
+    squares = []
+    side = img.shape[:1]
+    side = side[0] / 9
+    for i in range(9):
+        for j in range(9):
+            p1 = (i * side, j * side)
+            p2 = ((i + 1) * side, (j + 1) * side)
+            squares.append((p1, p2))
+    return squares
 
-	squares = []
-	side = img.shape[:1]
-	side = side[0] / 9
-	for i in range(9):
-		for j in range(9):
-			p1 = (i * side, j * side)
-			p2 = ((i + 1) * side, (j + 1) * side)
-			squares.append((p1, p2))
-	return squares
 
 def showImage(img):
-	cv2.imshow('Grids', img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+    cv2.imshow('Grids', img)
+    cv2.waitKey(200)
+    cv2.destroyAllWindows()
+
 
 skip_dilate = False
 
@@ -75,15 +79,27 @@ if not skip_dilate:
 
 corners = findCorners(proc)
 cropped = crop(original, corners)
+# cropped = crop(proc, corners)
 squares = gridDetect(cropped)
-print(squares)
-displayRects(cropped, squares)
+# print(squares)
+# displayRects(cropped, squares)
 
 croppedImages = []
 
-for x in range(0,81):
-	croppedImages.append(cropped[int(squares[x][0][0]):int(squares[x][1][0]), int(squares[x][0][1]):int(squares[x][1][1])])
+for x in range(0, 81):
+    mini_img = cropped[int(squares[x][0][0]):int(squares[x][1][0]), int(squares[x][0][1]):int(squares[x][1][1])]
+    m_img = cv2.resize(mini_img, (28, 28), interpolation=cv2.INTER_AREA)
+    m_bin = cv2.adaptiveThreshold(m_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    croppedImages.append(m_bin)
 
-#croppedImages contains the 81 extracted images for use in MNIST!
-#example
+print len(croppedImages)
+print type(croppedImages[6])
+print croppedImages[6].shape
+print np.unique(croppedImages[6])
+
+# croppedImages contains the 81 extracted images for use in MNIST!
+# example
 showImage(croppedImages[6])
+
+for i in range(81):
+	showImage(croppedImages[i])
